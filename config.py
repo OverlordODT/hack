76f9bf16ee558ce7f440e1d2ad317c52117228a6
OverlordODT <artemvorobyshek11@gmail.com>
2025-08-30 20:26:41 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+# Конфигурация бота
+API_ID = 25246545  # Ваш API_ID
+API_HASH = 'd38717a1918c1f411aefec7e464ec296'  # Ваш API_HASH
+BOT_TOKEN = '7388822561:AAE_FEcKcHhxbCVno90o6AKWCxeCHeUAHGs'  # Токен бота от @BotFather
+
+# ID каналов (ПОЛОЖИТЕЛЬНЫЕ БЕЗ МИНУСА!)
+CHANNELS = {
+    "tgk": 1512971940,  # БЕЗ МИНУСА! из -1512971940
+    "chat": 1532492207,  # БЕЗ МИНУСА! из -1532492207  
+    "bio": 2776728872   # БЕЗ МИНУСА! из -2776728872
+}
+
+# Ссылки на каналы
+CHANNEL_LINKS = {
+    "tgk": "https://t.me/kitrite",
+    "chat": "https://t.me/kitritchat", 
+    "bio": "https://t.me/kitritpidor"
+}
